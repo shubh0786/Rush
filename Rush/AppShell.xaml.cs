afe6d4050a1cb2b08b434b021f8a4911ec34f01b
SHUBHAM SINGH <shubham.singh267@gmail.com>
2022-01-17 02:35:45 +1300

    Add project files.

---
+++ Rush/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+ 
+namespace Rush
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+          
+        }
+
+    }
+}
